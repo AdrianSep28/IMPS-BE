5b8afb9a5e5f9688906fda4ca8dcd827149296ff
Adrian Sepulveda <89766360+AdrianSep28@users.noreply.github.com>
2024-06-01 21:20:27 +0800

    a

--- IMPS-BE/IMPS/src/main/java/com/imps/IMPS/repositories/FileRepository.java
+++
@@ -1,9 +0,0 @@
-package com.imps.IMPS.repositories;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.imps.IMPS.models.File;
-
-public interface FileRepository extends CrudRepository<File, String> {
-
-}
